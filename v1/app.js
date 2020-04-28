d117afa5115c53e95ede0c802264c75b6c75126b
morbi <the_morbidus@hotmail.com>
2020-04-28 14:37:07 -0400

    add new routes to add campgrounds

--- v1/app.js
+++ v1/app.js
@@ -2,21 +2,40 @@ const PORT = process.env.PORT || 8600;
 
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+const campgrounds = [
+  {name: "Salmon Creek", image: "https://pixabay.com/get/57e1dd4a4350a514f1dc84609620367d1c3ed9e04e507441722673d19f4ec1_340.jpg"},
+  {name: "Granite Hill", image: "https://pixabay.com/get/57e1d14a4e52ae14f1dc84609620367d1c3ed9e04e507441722673d19f4ec1_340.jpg"},
+  {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e507441722673d19f4ec1_340.jpg"}
+];
 
 app.get("/", function(req,res){
   res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-  const campgrounds = [
-    {name: "Salmon Creek", image: "https://pixabay.com/get/57e1dd4a4350a514f1dc84609620367d1c3ed9e04e507441722673d19f4ec1_340.jpg"},
-    {name: "Granite Hill", image: "https://pixabay.com/get/57e1d14a4e52ae14f1dc84609620367d1c3ed9e04e507441722673d19f4ec1_340.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e507441722673d19f4ec1_340.jpg"}
-  ];
+
 
   res.render("campgrounds",{campgrounds:campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+  const name = req.body.name;
+  const image = req.body.image;
+  let newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
+
+  res.redirect("campgrounds");
+  // get data from form and add to campgrounds array
+  // redirect back to campgrounds page
+});
+
+app.get("/campgrounds/new", function(req, res){
+  res.render("new.ejs");
 });
 
 app.listen(PORT, process.env.IP, function(){
