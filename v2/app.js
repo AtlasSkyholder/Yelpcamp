a52f7c2a72a395a1b9dd01ef80d50e6c3e5a8053
morbi <the_morbidus@hotmail.com>
2020-04-28 23:09:04 -0400

    v2 folder added for lesson purpose

---
+++ v2/app.js
@@ -0,0 +1,66 @@
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8600;
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const connectDB = require("./db/connection");
+const Camps = require("./db/schema");
+
+connectDB();
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+const campgrounds = [
+  {name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402_960_720.jpg"},
+  {name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2016/02/09/16/35/night-1189929_960_720.jpg"},
+  {name: "Mountain Goat's Rest", image: "https://cdn.pixabay.com/photo/2019/10/03/11/14/camp-4522970_960_720.jpg"},
+  {name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402_960_720.jpg"},
+  {name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2016/02/09/16/35/night-1189929_960_720.jpg"},
+  {name: "Mountain Goat's Rest", image: "https://cdn.pixabay.com/photo/2019/10/03/11/14/camp-4522970_960_720.jpg"},
+  {name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402_960_720.jpg"},
+  {name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2016/02/09/16/35/night-1189929_960_720.jpg"},
+  {name: "Mountain Goat's Rest", image: "https://cdn.pixabay.com/photo/2019/10/03/11/14/camp-4522970_960_720.jpg"}
+];
+
+app.get("/", function(req,res){
+  res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+
+
+  res.render("campgrounds",{campgrounds:campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+  const name = req.body.name;
+  const image = req.body.image;
+  let newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
+
+  //.save to database
+  const newCamp = new Camps(newCampground);
+
+  newCamp.save((error) => {
+    if (error) {
+      console.log('Ooops, something went wrong.');
+    } else {
+      console.log('Data has been save!!');
+    }
+  });
+
+
+  res.redirect("campgrounds");
+  // get data from form and add to campgrounds array
+  // redirect back to campgrounds page
+});
+
+app.get("/campgrounds/new", function(req, res){
+  res.render("new.ejs");
+});
+
+app.listen(PORT, process.env.IP, function(){
+  console.log("The YelpCamp Server Has Started!");
+});
